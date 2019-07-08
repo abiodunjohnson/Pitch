a7047e9f08221b8c72d74b9f7c92d28351afa7f6
Alabi johnson <abiodunalabijohn@gmail.com>
2019-07-08 17:44:51 +0100

    updateallworkdone

---
+++ resources/javascript/script.js
@@ -0,0 +1,14 @@
+
+/* Animation on scroll*/
+window.sr = ScrollReveal();
+sr.reveal('.photo-box', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+sr.reveal('.section-news', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
